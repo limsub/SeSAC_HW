f1c0d3f2bb34f40d0cf5320732a9e06600c5c63e
limsub <limsss9076@naver.com>
2023-07-24 17:20:44 +0900

    hw 1

--- 0724hw/0724hw/LEDBoardViewController.swift
+++ 0724hw/0724hw/LEDBoardViewController.swift
@@ -9,21 +9,112 @@ import UIKit
 
 class LEDBoardViewController: UIViewController {
 
+    // outlet
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var textColorButton: UIButton!
+    @IBOutlet var mainTextField: UITextField!
+    @IBOutlet var upperView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 초기 화면 레이블 Hello Swift
+        designMainLabel(mainLabel)
+        designButton(sendButton, name: "보내기")
+        designButton(textColorButton, name: "Aa")
+        
+        upperView.layer.cornerRadius = 10
+        upperView.backgroundColor = .white
+        
+        // 텍스트필드의 테두리를 없애기 위한 작업 -> 오래걸림;;
+        mainTextField.borderStyle = .none
+        mainTextField.textColor = .red
+        
+        
 
-        // Do any additional setup after loading the view.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /* 디자인함수 */
+    // 레이블
+    func designMainLabel(_ sender: UILabel) {
+        sender.text = "Hello Swift"
+        sender.textColor = .red
+        sender.textAlignment = .center
+        sender.font = UIFont.boldSystemFont(ofSize: 40)
+        sender.numberOfLines = 0
     }
-    */
+    // 버튼
+    func designButton(_ sender: UIButton, name: String) {
+        
+        sender.layer.cornerRadius = 10
+        sender.layer.borderColor = UIColor.black.cgColor
+        sender.layer.borderWidth = 1
+        
+        switch(name) {
+        case "보내기":
+            sender.tintColor = .black
+        case "Aa":
+            sender.tintColor = .red
+        default:
+            break;
+        }
+    }
+    
+    
+    /* 기능 함수 */
+    // 보내기 버튼 - 키보드 내려가기, 레이블 띄워주기
+    @IBAction func sendButtonTapped(_ sender: UIButton) {
+        // 키보드 내려가기
+        view.endEditing(true)
+        
+        // 레이블 띄워주기
+        mainLabel.text = mainTextField.text
+    }
+    
+    // 리턴키 - 키보드 내려가기, 레이블 띄워주기
+    @IBAction func keyboardReturnTapped(_ sender: UITextField) {    // Did End On Exit
+        // 키보드 내려가기
+        view.endEditing(true)
+        
+        // 레이블 띄워주기
+        mainLabel.text = mainTextField.text
+        
+    }
+    
+    // 배경 선택 시 - 키보드 내려가기 or 상단 뷰 토글
+    @IBAction func backgroundTapGesture(_ sender: UITapGestureRecognizer) {
+        // 키보드 올라와 있는 상태 -> 키보드 내리기
+        // 키보드 내려가 있는 상태 -> 상단 뷰 토글
+        
+        upperView.isHidden.toggle()
+        
+        // 현재 키보드가 올라와있는지에 대한 변수가 필요하다
+        view.endEditing(true)
+    }
+    
+    // 텍스트 컬러 버튼 누르면 색깔 랜덤으로 변경 -> textfield, 버튼, 레이블
+    @IBAction func colorButtonTapped(_ sender: UIButton) {
+        
+        // drand48() : [0.0, 1.0) 사이의 값을 반환
+        let rRed = CGFloat(drand48())
+        let rGreen = CGFloat(drand48())
+        let rBlue = CGFloat(drand48())
+        
+        let setColor = UIColor(red: rRed, green: rGreen, blue: rBlue, alpha: 1.0)
+        
+        mainTextField.textColor = setColor
+        textColorButton.tintColor = setColor
+        mainLabel.textColor = setColor
+        
+        
+    }
+    
+    
+    // 텍스트 컬러 버튼 누르면 상단 뷰 토글 처리 -> ???
+
 
 }
