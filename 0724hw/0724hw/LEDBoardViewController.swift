62441dcc5414c035ed00618a90860595f8d1076b
limsub <limsss9076@naver.com>
2023-07-24 16:27:32 +0900

    commit practice

---
+++ 0724hw/0724hw/LEDBoardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LEDBoardViewController.swift
+//  0724hw
+//
+//  Created by 임승섭 on 2023/07/24.
+//
+
+import UIKit
+
+class LEDBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
