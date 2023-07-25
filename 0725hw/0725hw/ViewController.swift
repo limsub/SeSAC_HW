8ad892f3bb72fdc8d82bdb6f50aa4b3935b83e68
limsub <limsss9076@naver.com>
2023-07-25 16:07:19 +0900

    7/25 homework start

---
+++ 0725hw/0725hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0725hw
+//
+//  Created by 임승섭 on 2023/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
