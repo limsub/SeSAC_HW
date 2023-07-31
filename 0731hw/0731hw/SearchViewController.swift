4dc94bcca1d0533f5e59e76b821303628df7ac00
limsub <limsss9076@naver.com>
2023-07-31 21:05:14 +0900

    7/31 homework 검색 기능 추가

--- 0731hw/0731hw/SearchViewController.swift
+++ 0731hw/0731hw/SearchViewController.swift
@@ -7,15 +7,79 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        if self.isFiltering {
+            cell.textLabel?.text = self.filteredArr[indexPath.row]
+        }
+        else {
+            cell.textLabel?.text = self.arr[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if self.isFiltering {
+            return self.filteredArr.count
+        }
+        else {
+            return self.arr.count
+        }
+        
+    }
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        // 영어 검색할 수도 있으니까 lowercased
+        guard let text = searchController.searchBar.text?.lowercased() else {return}
+        self.filteredArr = self.arr.filter { $0.lowercased().contains(text) }
+        
+        
+        
+        self.movieTableView.reloadData()
+        
+    }
+}
 
+class SearchViewController: UIViewController {
+    
+    
+    @IBOutlet weak var movieTableView: UITableView!
+    
+    var arr: [String] = [];
+    
+    // var arr = ["Zedd", "Alan Walker", "David Guetta", "Avicii", "Marshmello", "Steve Aoki", "R3HAB", "Armin van Buuren", "Skrillex", "Illenium", "The Chainsmokers", "Don Diablo", "Afrojack", "Tiesto", "KSHMR", "DJ Snake", "Kygo", "Galantis", "Major Lazer", "Vicetone"]
+    
+    var filteredArr: [String] = []
+    
+    
+    
+    // 고려해야 하는 것 : searchBar가 활성화 되어 있는지, text가 입력이 되어있는지
+    var isFiltering: Bool {
+        let searchController = self.navigationItem.searchController
+        let isActive = searchController?.isActive ?? false
+        let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false
+        
+        return isActive && isSearchBarHasText
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for m in MovieInfo().movie {
+            arr.append(m.title)
+        }
 
         title = "검색 화면"
-        
-        
-        
         navigationItem.leftBarButtonItem = UIBarButtonItem(
             image: UIImage(systemName: "xmark"),
             style: .plain,
@@ -23,6 +87,27 @@ class SearchViewController: UIViewController {
             action: #selector(closeButtonTapped)
         )
         navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        
+        
+        setupSearchController()
+        setupTableView()
+    }
+    
+    
+    
+    // set up search controller
+    func setupSearchController() {
+        let searchController = UISearchController(searchResultsController: nil)
+        self.navigationItem.searchController = searchController
+        
+        // text가 업데이트될 때마다 불리는 메소드
+        searchController.searchResultsUpdater = self
+    }
+    // set up tableview
+    func setupTableView() {
+        self.movieTableView.delegate = self
+        self.movieTableView.dataSource = self
     }
     
     @objc
