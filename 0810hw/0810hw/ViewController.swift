8edeb3c30e05c2f36d4399d31d9f168db9a2717b
limsub <limsss9076@naver.com>
2023-08-10 14:21:13 +0900

    commit

---
+++ 0810hw/0810hw/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  0810hw
+//
+//  Created by 임승섭 on 2023/08/10.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet var firstTextView: UITextView!
+    
+    @IBOutlet var secondTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
