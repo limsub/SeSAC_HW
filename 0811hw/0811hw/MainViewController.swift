c8c32aba21066e12d399263054131551f5d8ef29
limsub <limsss9076@naver.com>
2023-08-11 21:34:46 +0900

    8/11 APIManager 사용 (MainViewController - 장르 api, 트렌드 api)

--- 0811hw/0811hw/MainViewController.swift
+++ 0811hw/0811hw/MainViewController.swift
@@ -32,47 +32,70 @@ class MainViewController: UIViewController {
     }
     
     func callRequest2() {
-        let url = "https://api.themoviedb.org/3/genre/movie/list"
         
-        let header: HTTPHeaders = ["Authorization" : APIKey.tmdb]
-        
-        
-        // SwiftyJSON : Work with Alamofire
-        AF.request(url, method: .get, headers: header)
-            .validate()
-            .responseJSON { response in
-                switch response.result {
-                case .success(let value):
-                    let json = JSON(value)
-                    print("JSON22: \(json)")
+        APIManager.shared.callRequest(.movieGenre, 0) { json in
+            // 3중 loop...?
+            for (index, movie) in self.movieList.enumerated() {
+                print(index, movie.genre)
+                self.movieList[index].genreString.removeAll()
+                
+                for movieGenre in movie.genre {
                     
-                    
-                    // 3중 loop...?
-                    for (index, movie) in self.movieList.enumerated() {
-                        print(index, movie.genre)
-                        self.movieList[index].genreString.removeAll()
+                    for g in json["genres"].arrayValue {
                         
-                        for movieGenre in movie.genre {
-                            
-                            for g in json["genres"].arrayValue {
-                                
-                                if g["id"].intValue == movieGenre {
-                                    self.movieList[index].genreString.append(g["name"].stringValue)
-                                }
-                                print(index, self.movieList[index].genreString)
-                            }
+                        if g["id"].intValue == movieGenre {
+                            self.movieList[index].genreString.append(g["name"].stringValue)
                         }
+                        print(index, self.movieList[index].genreString)
                     }
-                    
-                    self.mainTableView.reloadData()
-                    
-                    
-                    
-                    
-                case .failure(let error):
-                    print(error)
                 }
             }
+            
+            self.mainTableView.reloadData()
+        }
+        
+        
+//        let url = "https://api.themoviedb.org/3/genre/movie/list"
+//
+//        let header: HTTPHeaders = ["Authorization" : APIKey.tmdb]
+//
+//
+//        // SwiftyJSON : Work with Alamofire
+//        AF.request(url, method: .get, headers: header)
+//            .validate()
+//            .responseJSON { response in
+//                switch response.result {
+//                case .success(let value):
+//                    let json = JSON(value)
+//                    print("JSON22: \(json)")
+//
+//
+//                    // 3중 loop...?
+//                    for (index, movie) in self.movieList.enumerated() {
+//                        print(index, movie.genre)
+//                        self.movieList[index].genreString.removeAll()
+//
+//                        for movieGenre in movie.genre {
+//
+//                            for g in json["genres"].arrayValue {
+//
+//                                if g["id"].intValue == movieGenre {
+//                                    self.movieList[index].genreString.append(g["name"].stringValue)
+//                                }
+//                                print(index, self.movieList[index].genreString)
+//                            }
+//                        }
+//                    }
+//
+//                    self.mainTableView.reloadData()
+//
+//
+//
+//
+//                case .failure(let error):
+//                    print(error)
+//                }
+//            }
 
     }
     
@@ -80,54 +103,88 @@ class MainViewController: UIViewController {
     
     // 일단 여기서 호출해보고, 다른 파일에 옮겨서 completion handler 사용하자
     func callRequest(_  completion: @escaping () -> Void) {
-        // 최종 url
-        let url = "https://api.themoviedb.org/3/trending/movie/week?language=en-US"
         
-        // header (HTTPHeader 아님!!)
-        let header: HTTPHeaders = ["Authorization" : APIKey.tmdb]
-        
-        // SwiftyJSON : Work with Alamofire
-        AF.request(url, method: .get, headers: header)
-            .validate()
-            .responseJSON { response in
-                switch response.result {
-                case .success(let value):
-                    let json = JSON(value)
-                    //print("JSON: \(json)")
-                    
-                    for item in json["results"].arrayValue {
-                        print(item)
-                        
-                        let date = item["release_date"].stringValue
-                        // let genre = item["genre_ids"].arrayValue // 일단 스트링으로
-                        var genre: [Int] = []
-                        for g in item["genre_ids"].arrayValue {
-                            genre.append(g.intValue)
-                        }
-                        
-                        let mainImage = item["poster_path"].stringValue
-                        let backImage = item["backdrop_path"].stringValue   // 일단 이건 어떻게 하는건지 모르겠음 지금
-                        let rate = item["vote_average"].doubleValue
-                        let title = item["title"].stringValue
-                        
-                        let id = item["id"].intValue
-                        let overView = item["overview"].stringValue
-                        
-                        let newMovie = MovieForMain(id: id, date: date, genre: genre, genreString: [], mainImage: mainImage, backImage: backImage, rate: rate, title: title, overview: overView)
-                        
-                        self.movieList.append(newMovie)
-                    }
-                    
-                    completion()
-                    //print(self.movieList)
-                    self.mainTableView.reloadData()
-                    
-                    
-                    
-                case .failure(let error):
-                    print(error)
+        APIManager.shared.callRequest(.movieTrend, 0) { json in
+            for item in json["results"].arrayValue {
+                print(item)
+                
+                let date = item["release_date"].stringValue
+                // let genre = item["genre_ids"].arrayValue // 일단 스트링으로
+                var genre: [Int] = []
+                for g in item["genre_ids"].arrayValue {
+                    genre.append(g.intValue)
                 }
+                
+                let mainImage = item["poster_path"].stringValue
+                let backImage = item["backdrop_path"].stringValue   // 일단 이건 어떻게 하는건지 모르겠음 지금
+                let rate = item["vote_average"].doubleValue
+                let title = item["title"].stringValue
+                
+                let id = item["id"].intValue
+                let overView = item["overview"].stringValue
+                
+                let newMovie = MovieForMain(id: id, date: date, genre: genre, genreString: [], mainImage: mainImage, backImage: backImage, rate: rate, title: title, overview: overView)
+                
+                self.movieList.append(newMovie)
+            }
+            
+            completion()
+            //print(self.movieList)
+            self.mainTableView.reloadData()
         }
+        
+        
+        
+        
+        
+//        // 최종 url
+//        let url = "https://api.themoviedb.org/3/trending/movie/week?language=en-US"
+//
+//        // header (HTTPHeader 아님!!)
+//        let header: HTTPHeaders = ["Authorization" : APIKey.tmdb]
+//
+//        // SwiftyJSON : Work with Alamofire
+//        AF.request(url, method: .get, headers: header)
+//            .validate()
+//            .responseJSON { response in
+//                switch response.result {
+//                case .success(let value):
+//                    let json = JSON(value)
+//                    //print("JSON: \(json)")
+//
+//                    for item in json["results"].arrayValue {
+//                        print(item)
+//
+//                        let date = item["release_date"].stringValue
+//                        // let genre = item["genre_ids"].arrayValue // 일단 스트링으로
+//                        var genre: [Int] = []
+//                        for g in item["genre_ids"].arrayValue {
+//                            genre.append(g.intValue)
+//                        }
+//
+//                        let mainImage = item["poster_path"].stringValue
+//                        let backImage = item["backdrop_path"].stringValue   // 일단 이건 어떻게 하는건지 모르겠음 지금
+//                        let rate = item["vote_average"].doubleValue
+//                        let title = item["title"].stringValue
+//
+//                        let id = item["id"].intValue
+//                        let overView = item["overview"].stringValue
+//
+//                        let newMovie = MovieForMain(id: id, date: date, genre: genre, genreString: [], mainImage: mainImage, backImage: backImage, rate: rate, title: title, overview: overView)
+//
+//                        self.movieList.append(newMovie)
+//                    }
+//
+//                    completion()
+//                    //print(self.movieList)
+//                    self.mainTableView.reloadData()
+//
+//
+//
+//                case .failure(let error):
+//                    print(error)
+//                }
+//        }
     }
     
 
