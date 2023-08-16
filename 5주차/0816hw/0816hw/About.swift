2005bd9154080db51479f94e26e8b717ac8f0f09
limsub <limsss9076@naver.com>
2023-08-16 16:15:53 +0900

    8/16 .gitignore 파일 생성

---
+++ 5주차/0816hw/0816hw/About.swift
@@ -0,0 +1,8 @@
+//
+//  About.swift
+//  0816hw
+//
+//  Created by 임승섭 on 2023/08/16.
+//
+
+import Foundation
---
+++ 5주차/0816hw/0816hw/VideoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VideoViewController.swift
+//  0816hw
+//
+//  Created by 임승섭 on 2023/08/16.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
